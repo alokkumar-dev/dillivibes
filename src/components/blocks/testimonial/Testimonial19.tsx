5adb483d2f16a11d663fb87122fbe9a1102473dd
alokkumar-dev <alokshakya2601@gmail.com>
2024-07-29 21:27:15 +0530

    add testimonials with their avatar

--- src/components/blocks/testimonial/Testimonial19.tsx
+++ src/components/blocks/testimonial/Testimonial19.tsx
@@ -3,6 +3,7 @@ import Carousel from 'components/reuseable/Carousel';
 import carouselBreakpoints from 'utils/carouselBreakpoints';
 // -------- data -------- //
 import { testimonialList7 } from 'data/testimonial-list';
+import Image from 'next/image';
 
 const Testimonial19: FC = () => {
   return (
@@ -16,18 +17,18 @@ const Testimonial19: FC = () => {
 
           <div className="swiper-container dots-closer dots-light dots-light-75">
             <Carousel grabCursor spaceBetween={0} navigation={false} breakpoints={carouselBreakpoints}>
-              {testimonialList7.map(({ id, name, designation, review }) => (
+              {testimonialList7?.map(({ id, name, avatar, review }) => (
                 <div className="item-inner" key={id}>
-                  <div className="card border-0 bg-white-900">
+                  <div className="card border-0 bg-white-900 testimonials-card-body">
                     <div className="card-body">
                       <span className="ratings five mb-3" />
                       <blockquote className="border-0 mb-0">
                         <p>“{review}”</p>
-                        <div className="blockquote-details">
+                        <div className="blockquote-details testimonials-details">
                           <div className="info p-0">
                             <h5 className="mb-0">{name}</h5>
-                            <p className="mb-0">{designation}</p>
                           </div>
+                          <Image src={avatar} className='testimonial-avatar' alt="avatar" width={43} height={43} />
                         </div>
                       </blockquote>
                     </div>
--- src/data/testimonial-list.ts
+++ src/data/testimonial-list.ts
@@ -1,26 +1,22 @@
 export const testimonialList7 = [
   {
-    id: 1,
-    name: 'Amit and Meera',
-    designation: 'Manager',
-    review: `Wonderful stay! Clean rooms, friendly staff, and great location. Exceeded our expectations. Highly recommend for a comfortable!.`
+    id: 3,
+    name: 'Anshuman Choudhary',
+    avatar: '/img/testimonial/t1.png',
+    review: `This place is really good…. Food portion served here was quite good… we ordered special thali which was very good… staff was very supportive… best part was Neat and Clean washroom on the highway… overall atmosphere and interior of this place is quite spacious...`
   },
+ 
   {
     id: 2,
-    name: 'Rahul and Anjali',
-    designation: 'Marketing Specialist',
-    review: `Amazing food and fantastic service! The ambiance was cozy, and every dish was delicious  and good services as well. A must-visit for food lovers!.`
+    name: 'Vandana Rao',
+    avatar: '/img/testimonial/t2.png',
+    review: `This place is a 10 on 10. I just can’t stop myself from talking about this wonderful place. This place have really affordable price without compromising on the quality and taste of the food. I accidentally found this place because of RS yadav bus travellers...`
   },
   {
-    id: 3,
-    name: 'Rohit and Priya',
-    designation: 'Sales Specialist',
-    review: `Perfect getaway! Comfortable rooms, excellent service, and stunning views. The staff made us feel truly welcome. Will definitely return!`
+    id: 1,
+    name: 'Komal Kumar Pal',
+    avatar: '/img/testimonial/t3.png',
+    review: `Dining at this pure vegetarian restaurant was an excellent experience. The menu offers a wide variety of vegetarian options. There is ample parking space, and an EV charging point is also available. The washrooms were in excellent condition. The food was delicious...`
   },
-  {
-    id: 4,
-    name: 'Neha',
-    designation: 'Food blogger',
-    review: `Delicious food, great atmosphere, and friendly staff. The desserts were especially amazing. Can't wait to come back!.`
-  },
+  
 ];
